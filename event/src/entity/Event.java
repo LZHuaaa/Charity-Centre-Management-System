63482b677e1760fabb4ce2352664f02fa9157c54
eyong <eyong@LAPTOP-LHTR8P1R>
2024-09-05 22:39:23 +0800

    add entity

---
+++ event/src/entity/Event.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author eyong
+ */
+import java.util.Objects;  
+
+public class Event implements Comparable<Event> {  
+    private String id;  
+    private String name;  
+    private String date; // Consider using LocalDate for more robust date handling  
+    private String location;  
+    private String description;  
+
+    public Event() {  
+    }  
+
+    public Event(String id, String name, String date, String location, String description) {  
+        this.id = id;  
+        this.name = name;  
+        this.date = date;  
+        this.location = location;  
+        this.description = description;  
+    }  
+
+    // Getters and Setters  
+    public String getId() {  
+        return id;  
+    }  
+
+    public void setId(String id) {  
+        this.id = id;  
+    }  
+
+    public String getName() {  
+        return name;  
+    }  
+
+    public void setName(String name) {  
+        this.name = name;  
+    }  
+
+    public String getDate() {  
+        return date;  
+    }  
+
+    public void setDate(String date) {  
+        this.date = date;  
+    }  
+
+    public String getLocation() {  
+        return location;  
+    }  
+
+    public void setLocation(String location) {  
+        this.location = location;  
+    }  
+
+    public String getDescription() {  
+        return description;  
+    }  
+
+    public void setDescription(String description) {  
+        this.description = description;  
+    }  
+
+    @Override  
+    public String toString() {  
+        return "Event{" +  
+                "id='" + id + '\'' +  
+                ", name='" + name + '\'' +  
+                ", date='" + date + '\'' +  
+                ", location='" + location + '\'' +  
+                ", description='" + description + '\'' +  
+                '}';  
+    }  
+
+    @Override  
+    public boolean equals(Object obj) {  
+        if (this == obj) return true;  
+        if (obj == null || getClass() != obj.getClass()) return false;  
+        Event event = (Event) obj;  
+        return id.equals(event.id);  
+    }  
+
+    @Override  
+    public int hashCode() {  
+        return Objects.hash(id);  
+    }  
+
+    @Override  
+    public int compareTo(Event o) {  
+        return this.date.compareTo(o.date); // Compare by date  
+    }  
+}  
---
+++ event/src/entity/Volunteer.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author eyong
+ */
+import java.util.Objects;  
+import adt.*;
+
+public class Volunteer implements Comparable<Volunteer> {  
+    private String id;  
+    private String name;  
+    private String phoneNumber;  
+    private String email;  
+    private ArrayList<Event> events; // List of events the volunteer is associated with  
+
+    public Volunteer() {  
+        this.events = new ArrayList<>();  
+    }  
+
+    public Volunteer(String id, String name, String phoneNumber, String email) {  
+        this.id = id;  
+        this.name = name;  
+        this.phoneNumber = phoneNumber;  
+        this.email = email;  
+        this.events = new ArrayList<>();  
+    }  
+
+    // Getters and Setters  
+    public String getId() {  
+        return id;  
+    }  
+
+    public void setId(String id) {  
+        this.id = id;  
+    }  
+
+    public String getName() {  
+        return name;  
+    }  
+
+    public void setName(String name) {  
+        this.name = name;  
+    }  
+
+    public String getPhoneNumber() {  
+        return phoneNumber;  
+    }  
+
+    public void setPhoneNumber(String phoneNumber) {  
+        this.phoneNumber = phoneNumber;  
+    }  
+
+    public String getEmail() {  
+        return email;  
+    }  
+
+    public void setEmail(String email) {  
+        this.email = email;  
+    }  
+
+    public ArrayList<Event> getEvents() {  
+        return events;  
+    }  
+
+    public void setEvents(ArrayList<Event> events) {  
+        this.events = events;  
+    }  
+
+    // Methods  
+    public void addEvent(Event event) {  
+        events.add(event);  
+    }  
+
+    public void removeEvent(Event event) {  
+        events.remove(event);  
+    }  
+
+    public void updateEvent(Event oldEvent, Event newEvent) {  
+        int index = events.indexOf(oldEvent);  
+        if (index != -1) {  
+            events.replace(index, newEvent);  
+        }  
+    }  
+
+    @Override  
+    public String toString() {  
+        return "Volunteer{" +  
+                "id='" + id + '\'' +  
+                ", name='" + name + '\'' +  
+                ", phoneNumber='" + phoneNumber + '\'' +  
+                ", email='" + email + '\'' +  
+                ", events=" + events +  
+                '}';  
+    }  
+
+    @Override  
+    public boolean equals(Object obj) {  
+        if (this == obj) return true;  
+        if (obj == null || getClass() != obj.getClass()) return false;  
+        Volunteer volunteer = (Volunteer) obj;  
+        return id.equals(volunteer.id);  
+    }  
+
+    @Override  
+    public int hashCode() {  
+        return Objects.hash(id);  
+    }  
+
+    @Override  
+    public int compareTo(Volunteer o) {  
+        return this.name.compareTo(o.name);  
+    }  
+}  
